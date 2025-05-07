4856947be8471a0bdcc602cf4b617187e0d37914
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-05-08 00:06:52 +0800

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ cd build-scripts
 cmake $RECIPE_DIR/scripts
 cd ..
 
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX FCFLAGS="-O2 -std=legacy" CFLAGS="-O2"
 
 make -j$(nproc)
 make install
