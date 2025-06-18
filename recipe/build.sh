5e457be1f1936d2fb6ece028e1efb3a859f022c7
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-06-18 22:07:55 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/bash
 
+# get the 
+git clone https://gitlab.com/hepcedar/lhapdf
+mv lhapdf/migration $PREFIX/share/lhapdf/migration
+
 FFLAGS="-std=legacy" ./configure --prefix=$PREFIX
 
 make -j$(nproc)
