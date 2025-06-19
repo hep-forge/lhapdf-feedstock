dd083b667c11356c17777f22e6e0d486b9fd24b3
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-06-19 11:54:40 +0900

    update script and meta

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,36 @@
 #! /usr/bin/bash
 
-FFLAGS="-std=legacy" ./configure --prefix=$PREFIX
+mkdir build-scripts
+cd build-scripts
+
+cmake $RECIPE_DIR/scripts
+cd ..
+
+autoreconf -i
+./configure --prefix=$PREFIX FCFLAGS="-O2 -std=legacy" CFLAGS="-O2"
+
 make -j$(nproc)
 make install
 
-git clone https://gitlab.com/hepcedar/lhapdf
-find lhapdf/migration -type f -exec sed -i 's/(__doc__)\()/g' {} +
-mv lhapdf/migration/* $PREFIX/bin
+mv $PREFIX/bin/lhapdf-config $PREFIX/bin/lhapdf5-config
+mv $PREFIX/include/LHAPDF/LHAPDF.h $PREFIX/include/LHAPDF/LHAPDF5.h
+mv $PREFIX/lib/libLHAPDF.a $PREFIX/lib/libLHAPDF5.a
+mv $PREFIX/lib/libLHAPDF.so $PREFIX/lib/libLHAPDF5.so
+
+# check python2.x is used — if so, install the migration scripts
+if command -v python >/dev/null 2>&1 && \
+   [ "$(python -c 'import sys; sys.stdout.write(str(sys.version_info[0]))')" -eq 2 ]; then
+
+  git clone https://gitlab.com/hepcedar/lhapdf
+  cp -R lhapdf/migration $PREFIX/share/lhapdf
+  find lhapdf/migration -type f \
+       -exec sed -i 's/libLHAPDF\./libLHAPDF5\./g' {} +
+  find lhapdf/migration -type f \
+       -exec sed -i 's/usage=\(__doc__\)//g' {} +
+  cp lhapdf/migration/cmpplotv5v6 $PREFIX/bin
+  cp lhapdf/migration/creategrids  $PREFIX/bin
+  cp lhapdf/migration/lhapdf5to6   $PREFIX/bin
+  cp lhapdf/migration/tomigrate    $PREFIX/bin
+  cp lhapdf/migration/cmpv5v6      $PREFIX/bin
+  cp lhapdf/migration/plotv5v6     $PREFIX/bin
+fi
